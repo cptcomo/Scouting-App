ab02e72144464d04fa4c7627d7ccea544da44bdd
Kevin Ngo <Kevin.ngo0407@gmail.com>
2016-02-20 10:34:58 -0500

    Version 1.0.3

--- app/src/main/java/com/example/drago/testapplication/MainActivity.java
+++ app/src/main/java/com/example/drago/testapplication/MainActivity.java
@@ -103,7 +103,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
